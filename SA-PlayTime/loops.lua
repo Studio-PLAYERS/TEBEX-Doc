979b8c36eda403039adfbbfd22bc758c557f2624
Dragodast <dragodastdragos@gmail.com>
2022-07-04 11:22:29 +0200

    Edited for SA-PlayTime

--- SA-PlayTime/loops.lua
+++ SA-PlayTime/loops.lua
@@ -4,6 +4,7 @@ CreateThread(function()
         if LocalPlayer.state.isLoggedIn then
             sleep = (1000 * 60) * QBCore.Config.UpdateInterval
             TriggerServerEvent('QBCore:UpdatePlayer')
+            TriggerServerEvent('PlayTime:Server:Increase')
         end
         Wait(sleep)
     end
